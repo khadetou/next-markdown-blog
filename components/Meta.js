a29943ef2fdda62ee52cf0115da1862c01edbdd9
khadetou <khadetou96@gmail.com>
2021-07-24 13:35:58 +0000

    starter

---
+++ components/Meta.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+const Meta = ({ title, keywords, description }) => {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="keywords" content={keywords} />
+      <meta name="description" content={description} />
+      <meta charSet="utf-8" />
+      <link rel="ico" href="/favicon.ico" />
+      <title>{title}</title>
+    </Head>
+  );
+};
+
+Meta.defaultProps = {
+  title: "Blog",
+  keywords: "Web development programming",
+  description: "Get the latest news in thi blog",
+};
